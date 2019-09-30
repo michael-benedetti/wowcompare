91097ddf66ff975f42f807d2c65c214b3089908b
Michael Benedetti <benedettim@forcenexus.io>
2019-09-30 11:05:51 -0400

    Fixed tooltip item name styling

--- client/src/ItemTooltip.tsx
+++ client/src/ItemTooltip.tsx
@@ -9,23 +9,48 @@ interface ItemTooltipProps {
   slot: string;
 }
 
-interface TooltipContainerProps {
-  item: Item;
-}
-
-const TooltipContainer = styled.div<TooltipContainerProps>`
-  & .ItemName {
-    font-size: 16x;
-    color: ${props => ITEM_QUALITY_COLORS[props.item.quality]};
+const TooltipContainer = styled.div`
+  & .Quality-0 {
+    color: ${ITEM_QUALITY_COLORS[0]};
+  }
+  
+  & .Quality-1 {
+    color: ${ITEM_QUALITY_COLORS[1]};
+  }
+  
+  & .Quality-2 {
+    color: ${ITEM_QUALITY_COLORS[2]};
+  }
+  
+  & .Quality-3 {
+    color: ${ITEM_QUALITY_COLORS[3]};
+  }
+  
+  & .Quality-4 {
+    color: ${ITEM_QUALITY_COLORS[4]};
+  }
+  
+  & .Quality-5 {
+    color: ${ITEM_QUALITY_COLORS[5]};
+  }
+  
+  & .Quality-6 {
+    color: ${ITEM_QUALITY_COLORS[6]};
+  }
+  
+  & .Quality-7 {
+    color: ${ITEM_QUALITY_COLORS[7]};
+  }
+  
+  & .Quality-8 {
+    color: ${ITEM_QUALITY_COLORS[8]};
   }
   `;
 
 const ItemTooltip: React.FC<ItemTooltipProps> = (props: ItemTooltipProps) => {
   return (
-    <TooltipContainer
-      item={props.item}
-    >
-      <div className={"ItemName"}>{props.item.name}</div>
+    <TooltipContainer>
+      <div className={`Quality-${props.item.quality}`}>{props.item.name}</div>
       <div>{`Item Level ${props.item.itemLevel}`}</div>
       <div>{ITEM_TYPES[props.slot]}</div>
       {props.item.stats.map((stat) => {
